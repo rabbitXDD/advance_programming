b1c234d03ce2ceac8f9e1041d63b6c9e2e5f19a8
李明緯 <Ming@limingweideMacBook-Pro.local>
2016-06-19 22:51:05 +0800

    aaa

---
+++ jsonTest.py
@@ -0,0 +1,37 @@
+import json
+import itertools
+from pprint import pprint
+
+# -*- coding: utf8 -*-
+with open('keyword.json') as data_file:    
+    data = json.load(data_file)
+    print type(data)
+
+    nameList = []
+    sizeList = []
+    jsonList = []
+    for ele in data.keys():
+        name = 'name', ele.encode('utf8')
+        nameList.append(name)
+
+    for ele in data.values():
+        size = 'size',ele   
+        sizeList.append(size)
+
+    a = nameList[5][1]
+    print a
+
+    for x in range(0, len(nameList)):
+        aaa = nameList[x][1].decode('utf8')
+        ele = {nameList[x][0]:aaa, sizeList[x][0]:sizeList[x][1]}
+        jsonList.append(ele)
+
+    #print jsonList
+    jsonData = json.dumps(jsonList)
+   # print jsonData
+    with open('aaa.json', 'w') as f:
+        for ele in jsonData:
+            f.write(ele)
+
+
+
